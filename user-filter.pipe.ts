bd336103e6ab8fe7210636b84ad739997cfbbeb5
trushalip2 <trushalip@kpit.com>
2017-08-04 10:46:04 +0530

    Filters data based on string passed

---
+++ user-filter.pipe.ts
@@ -0,0 +1,18 @@
+  
+    import {PipeTransform,Pipe} from '@angular/core';
+    import {IUser} from './user';
+
+    @Pipe({
+        name:'userFilter'
+    })
+  
+  
+  export class UserFilterPipe implements PipeTransform{
+      transform(value: IUser[],filterBy: string): IUser[] {
+
+            filterBy= filterBy ? filterBy.toLocaleLowerCase():null;
+          return filterBy ? value.filter((user:IUser)=>
+          user.fullName.toLocaleLowerCase().indexOf(filterBy)!== -1): value;
+      }
+
+  }
